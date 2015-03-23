691cf7be450ddccc666249e1e04e2652cd485dd7
vlabo <vladdocan@gmail.com>
2015-03-23 09:26:56 +0200

    Qt Test

--- source/main.cpp
+++ source/main.cpp
@@ -3,13 +3,11 @@
 
 void init() {
 	SetGpioFunction(47, 1);
-    int i = 10;
-    i++;
 	while(true) {
 		SetGpio(47, 1);
-		Wait(1000000);
+        Wait(500000);
 		SetGpio(47, 0);
-		Wait(1000000);
+        Wait(500000);
 	}
 }
 
